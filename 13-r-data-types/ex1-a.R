cbf8d0c304d3e501971a90d0496f8fd73064a792
simondzs <dora.simon@econ.uzh.ch>
2018-09-05 08:16:20 +0200

    added first R part

---
+++ 13-r-data-types/ex1-a.R
@@ -0,0 +1,12 @@
+# This is an example for using scripts in class
+
+a<-"Hello"
+b<-"Class"
+
+a
+b
+
+# Try what happens when you click on Run, 
+# click on the other icon between Run and Source
+# And when you hit ctrl+Enter, 
+# ctrl+Enter on highlighted code
